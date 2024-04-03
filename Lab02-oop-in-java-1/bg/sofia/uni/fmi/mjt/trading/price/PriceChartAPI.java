b673f71c2c41a5ecb19d7a8cd37db35d5a33295a
Venelina Andreeva <44705364+venyyan@users.noreply.github.com>
2024-04-03 19:01:14 +0300

    Delete Lab02-oop-in-java-1 directory

--- Lab02-oop-in-java-1/bg/sofia/uni/fmi/mjt/trading/price/PriceChartAPI.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.trading.price;
-
-public interface PriceChartAPI {
-    double getCurrentPrice(String stockTicker);
-
-    boolean changeStockPrice(String stockTicker, int percentChange);
-}
